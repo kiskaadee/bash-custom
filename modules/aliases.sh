029020655e01d53a9d60a4d7604459806612ee3f
kiskaadee <fcortesbio@gmail.com>
2026-03-17 16:28:29 -0500

    fix: corrected typos

--- modules/aliases.sh
+++ modules/aliases.sh
@@ -3,10 +3,10 @@ alias src="source ~/.bashrc" # quick source
 alias grep='rg' # grep wrapper
 alias ls='eza --icons --group-directories-first' # ls wrapper
 alias ll='eza -la --icons --octal-permissions --git' # ll wrapper
-alias mute='swpctl set-mute @DEFAULT_AUDIO_SINK@ 1'
+alias mute='wpctl set-mute @DEFAULT_AUDIO_SINK@ 1'
 alias unmute='wpctl set-mute @DEFAULT_AUDIO_SINK@ 0'
-alias vol50='pctl set-volume @DEFAULT_AUDIO_SINK@ 0.5'
-alias fullvol='apctl set-volume @DEFAULT_AUDIO_SINK@ 1'
+alias vol50='wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.5'
+alias fullvol='wpctl set-volume @DEFAULT_AUDIO_SINK@ 1'
 
 # Shortcuts
 
