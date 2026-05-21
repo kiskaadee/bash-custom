500274d6f2ccca4f4d09d355131da890e6826484
kiskaadee <fcortesbio@gmail.com>
2026-05-21 11:14:00 -0500

    feat(core): add direnv shell hook for automatic environment loading

--- profile/30-autostart.sh
+++ profile/30-autostart.sh
@@ -2,9 +2,11 @@
 if [ -n "$BASH_VERSION" ]; then
     eval "$(starship init bash)"
     eval "$(zoxide init bash)"
+    eval "$(direnv hook bash)"
 elif [ -n "$ZSH_VERSION" ]; then
     eval "$(starship init zsh)"
     eval "$(zoxide init zsh)"
+    eval "$(direnv hook zsh)"
 fi
 
 # --- Visual Entry ---
